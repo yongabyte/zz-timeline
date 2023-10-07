1edcd83099b14b1f028460f4ee19a3aa0d0b3eaf
yongabyte <jzeng.dev@gmail.com>
2023-10-06 22:58:46 -0400

    Save progress

---
+++ src/pages/index.tsx
@@ -0,0 +1,149 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import { Chrono } from "react-chrono";
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <Chrono
+      items={[
+        {
+          title: "May 1940",
+          cardTitle: "Dunkirk",
+          url: "http://google.com",
+          cardSubtitle:
+            "Men of the British Expeditionary Force (BEF) wade out to a destroyer during the evacuation from Dunkirk.",
+          cardDetailedText: `On 10 May 1940, Hitler began his long-awaited offensive in the west by invading neutral Holland and Belgium and attacking northern France. Holland capitulated after only five days of fighting, and the Belgians surrendered on 28 May. With the success of the German ‘Blitzkrieg’, the British Expeditionary Force and French troops were in danger of being cut off and destroyed.`,
+        },
+        {
+          title: "25 July 1940",
+          cardTitle: "The Battle of Britain",
+          cardSubtitle: `RAF Spitfire pilots scramble for their planes`,
+          cardDetailedText: `After France’s surrender in June 1940, Churchill told the British people, “Hitler knows that he will have to break us in this island or lose the war”. To mount a successful invasion, the Germans had to gain air superiority. The first phase of the battle began on 10 July with Luftwaffe attacks on shipping in the Channel.
+        The following month, RAF Fighter Command airfields and aircraft factories came under attack. Under the dynamic direction of Lord Beaverbrook, production of Spitfire and Hurricane fighters increased, and despite its losses in pilots and planes, the RAF was never as seriously weakened as the Germans supposed.`,
+        },
+        {
+          title: "June 1941",
+          cardTitle: "Operation Barbarossa",
+          cardSubtitle: `A column of Red Army prisoners taken during the first days of the German invasion`,
+          cardDetailedText: `Since the 1920s, Hitler had seen Russia, with its immense natural resources, as the principal target for conquest and expansion. It would provide, he believed, the necessary ‘Lebensraum’, or living space, for the German people. And by conquering Russia, Hitler would also destroy the “Jewish pestilential creed of Bolshevism”. His non-aggression pact with Stalin in August 1939 he regarded as a mere temporary expedient.
+        Barely a month after the fall of France, and while the Battle of Britain was being fought, Hitler started planning for the Blitzkrieg campaign against Russia, which began on 22 June 1941. Despite repeated warnings, Stalin was taken by surprise, and for the first few months the Germans achieved spectacular victories, capturing huge swathes of land and hundreds of thousands of prisoners. But they failed to take Moscow or Leningrad before winter set in.
+        On 5/6 December, the Red Army launched a counter-offensive which removed the immediate threat to the Soviet capital. It also brought the German high command to the brink of a catastrophic military crisis. Hitler stepped in and took personal command. His intervention was decisive and he later boasted, “That we overcame this winter and are today in a position again to proceed victoriously… is solely attributable to the bravery of the soldiers at the front and my firm will to hold out…”`,
+        },
+      ]}
+      mode="VERTICAL_ALTERNATING"
+    />
+  );
+  // return (
+  //   <main
+  //     className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+  //   >
+  //     <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+  //       <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+  //         Get started by editing&nbsp;
+  //         <code className="font-mono font-bold">src/pages/index.tsx</code>
+  //       </p>
+  //       <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+  //         <a
+  //           className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+  //           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+  //           target="_blank"
+  //           rel="noopener noreferrer"
+  //         >
+  //           By{' '}
+  //           <Image
+  //             src="/vercel.svg"
+  //             alt="Vercel Logo"
+  //             className="dark:invert"
+  //             width={100}
+  //             height={24}
+  //             priority
+  //           />
+  //         </a>
+  //       </div>
+  //     </div>
+
+  //     <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+  //       <Image
+  //         className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+  //         src="/next.svg"
+  //         alt="Next.js Logo"
+  //         width={180}
+  //         height={37}
+  //         priority
+  //       />
+  //     </div>
+
+  //     <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+  //       <a
+  //         href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+  //         className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         <h2 className={`mb-3 text-2xl font-semibold`}>
+  //           Docs{' '}
+  //           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+  //             -&gt;
+  //           </span>
+  //         </h2>
+  //         <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+  //           Find in-depth information about Next.js features and API.
+  //         </p>
+  //       </a>
+
+  //       <a
+  //         href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+  //         className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         <h2 className={`mb-3 text-2xl font-semibold`}>
+  //           Learn{' '}
+  //           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+  //             -&gt;
+  //           </span>
+  //         </h2>
+  //         <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+  //           Learn about Next.js in an interactive course with&nbsp;quizzes!
+  //         </p>
+  //       </a>
+
+  //       <a
+  //         href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+  //         className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         <h2 className={`mb-3 text-2xl font-semibold`}>
+  //           Templates{' '}
+  //           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+  //             -&gt;
+  //           </span>
+  //         </h2>
+  //         <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+  //           Discover and deploy boilerplate example Next.js&nbsp;projects.
+  //         </p>
+  //       </a>
+
+  //       <a
+  //         href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+  //         className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         <h2 className={`mb-3 text-2xl font-semibold`}>
+  //           Deploy{' '}
+  //           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+  //             -&gt;
+  //           </span>
+  //         </h2>
+  //         <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+  //           Instantly deploy your Next.js site to a shareable URL with Vercel.
+  //         </p>
+  //       </a>
+  //     </div>
+  //   </main>
+  // )
+}
