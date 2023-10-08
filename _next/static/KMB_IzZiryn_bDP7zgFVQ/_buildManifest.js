aca4f4fd89bd6e0930ced8dc60ce9d201b654de8
yongabyte <yongabyte@users.noreply.github.com>
2023-10-08 21:27:04 +0000

    Deploying to gh-pages from @ yongabyte/zz-timeline@998d5fb886bb95bbba2717ba89178fc3bd6c2db9 🚀

---
+++ _next/static/KMB_IzZiryn_bDP7zgFVQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/191-8386626e5fb56d2c.js","static/css/30b939724a221ef4.css","static/chunks/pages/index-b4a9336021dd29b2.js"],"/_error":["static/chunks/pages/_error-08a9db0f433628d8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/RXS7gNM8pZmMGsYv_oxBB/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/191-8386626e5fb56d2c.js","static/css/30b939724a221ef4.css","static/chunks/pages/index-49f9fab6f099b107.js"],"/_error":["static/chunks/pages/_error-08a9db0f433628d8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
