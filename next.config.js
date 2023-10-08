2e56f2de7afb9b97796b6de53aed49315dbd9565
yongabyte <yongabyte@users.noreply.github.com>
2023-10-08 18:51:15 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
 
 const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
 let assetPrefix = `/${repo}/`;
-// let basePath = `/${repo}`;
+let basePath = `/${repo}`;
 const nextConfig = {
   assetPrefix,
-  basePath: assetPrefix,
+  basePath,
   reactStrictMode: true,
   output: 'export',
   images: {
