dcb3cf2db9bfcae2bcf825f00cf27bd7b013a822
yongabyte <jzeng.dev@gmail.com>
2023-10-18 23:57:10 -0400

    mobile friendly

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { ChronoItemBuilder } from "@/lib/chronoItemBuilder";
 import { Chrono } from "react-chrono";
 import { Roboto } from 'next/font/google'
 import { useRouter } from 'next/router';
-
+import { useState } from 'react';
 const roboto = Roboto({
   weight: '400',
   subsets: ['latin'],
@@ -11,18 +11,23 @@ const roboto = Roboto({
 export default function Home() {
   const basePath: string = useRouter().basePath; //finding current bath path, in case of app lives on subdomain 
   const imageItems = new ChronoItemBuilder(basePath).buildchronoItems();
-  
+  let mHeight: string = "400";
+
+  if (typeof window !== "undefined" && window.innerWidth <= 768){
+    mHeight = "280";
+  }
+
   return (
     <main  
-    className={roboto.className}
+    className={roboto.className + ' -mx-4'}
     >
       <Chrono
         items={imageItems}
         mode="VERTICAL_ALTERNATING"
-        noUniqueId
-        mediaHeight="500"
-        cardWidth="500"
-        scrollable
+        mediaHeight={mHeight}
+        cardHeight={mHeight}
+        cardWidth={mHeight}
+        mediaSettings={{ align: 'left', fit: 'contain' }}
         theme={{
           primary: 'grey',
           secondary: 'white',
