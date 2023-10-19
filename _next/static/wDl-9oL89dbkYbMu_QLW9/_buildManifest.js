8316b19047a2050c49946a2fcdfae5fb55bea171
yongabyte <yongabyte@users.noreply.github.com>
2023-10-19 04:05:18 +0000

    Deploying to gh-pages from @ yongabyte/zz-timeline@17cf743c964d40c60c320d89ee01329f85c6505e 🚀

---
+++ _next/static/wDl-9oL89dbkYbMu_QLW9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/14-28bd2f637832c9e6.js","static/css/30b939724a221ef4.css","static/chunks/pages/index-722e816c14be5340.js"],"/_error":["static/chunks/pages/_error-08a9db0f433628d8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
